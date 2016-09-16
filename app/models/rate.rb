f2eeb1d45c160fbdc375069b62749e82f21909c9
Brian Swan <bgswan@gmail.com>
2016-09-17 08:16:49 +1000

    Added Rate.lookup

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,2 +1,8 @@
 class Rate < ActiveRecord::Base
+
+  def self.lookup(date, currency)
+    rate = self.where(rate_at: date, currency: currency).first
+    raise Exchange::Rates::NoRateError, "No rate for #{currency} at #{date}" if rate.nil?
+    rate.rate
+  end
 end
--- test/models/rate_test.rb
+++ test/models/rate_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class RateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "implements lookup method to use as exchange rate source" do
+    assert_equal rates(:usd).rate, Rate.lookup("2016-09-15", "USD")
+  end
+
+  test "raises NoRateError when no rate for date or currency" do
+    assert_raises Exchange::Rates::NoRateError do
+      Rate.lookup("2016-01-01", "JUNK")
+    end
+  end
+
 end
